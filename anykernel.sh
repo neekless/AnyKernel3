5d0c238cbc74c4c2f42620319dd5e92b0cc2a5ef
Nicholas <22426822+neekless@users.noreply.github.com>
2021-07-19 10:16:21 +0800

    anykernel: Nickel

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sultan Kernel Plus for the Pixel 2 and Pixel 2 XL
+kernel.string=Nickel Kernel for the Pixel 2 and Pixel 2 XL
 do.devicecheck=1
 do.modules=0
 do.systemless=1
